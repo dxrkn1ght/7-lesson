690cd776375a364e6c9717efef56a5e5070f9f57
DXRKN1GHT <komronbekzubaydullayev@gmail.com>
2024-12-04 21:20:47 +0500

    have some problems

--- programs/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-app_name = 'programs'
-urlpatterns = [
-    path('list/', views.program_list, name='list'),
-    path('create/', views.program_add, name='program_add'),
-]
